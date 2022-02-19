aaeef99d09a97a610ad8179887484754e100d1c1
Joydeep Kundu <kjoydeep5@gmail.com>
2022-02-19 16:41:41 +0530

    adding sign

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+
 import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-
+import SignInAndSignUpPage from './pages/sign-in-and-up/sign-in-and-up.component';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
 
     </div>
---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.componet";
+
+import './sign-in-and-up.style.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        SIGN IN
+        SIGN UP
+        <div>
+            <SignIn />
+        </div>
+    </div>
+
+);
+export default SignInAndSignUpPage;
